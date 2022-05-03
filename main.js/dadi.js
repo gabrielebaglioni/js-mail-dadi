8de4b004f801bbca51aa801089dc4324c32d9499
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-03 14:55:31 +0200

    genero due numeri randomici

--- main.js/dadi.js
+++ main.js/dadi.js
@@ -2,3 +2,11 @@
 //estraggo due numeri tyra il minimo e il massimo 
 // li comparo
 // il più grande vince
+
+
+const min = Number("1");
+const max = Number("6");
+const randomUtente = Math.floor(Math.random() * (max - min + 1)) + min;
+const randomComp = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomUtente);
+console.log(randomComp);
