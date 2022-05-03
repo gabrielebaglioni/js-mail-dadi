c1bb32c3d4a153dda2b51b1eab1e78b3da74b268
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-03 15:20:54 +0200

    finito logica dadi

--- main.js/dadi.js
+++ main.js/dadi.js
@@ -8,5 +8,14 @@ const min = Number("1");
 const max = Number("6");
 const randomUtente = Math.floor(Math.random() * (max - min + 1)) + min;
 const randomComp = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(randomUtente);
-console.log(randomComp);
+// console.log(randomUtente);
+// console.log(randomComp);
+if ( randomUtente > randomComp){
+   console.log("l'utente ha vinto")
+}
+else if ( randomUtente < randomComp){
+   console.log("il computer ha vinto")
+}
+else{
+   console.log("pareggio = ritirare una seconda volta il dado")
+}
