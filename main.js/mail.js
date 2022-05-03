8735bf053a71e7cfc5aa18b5795ca09e255d3f92
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-03 14:20:10 +0200

    prima parte mail fatta

--- main.js/mail.js
+++ main.js/mail.js
@@ -4,6 +4,12 @@
 //  Se l'email esiste l'utente è benvenuto 
 // Se non è registrato gli chiedo di fare in Sing Up
  
-
+const controllo = document.querySelector(".container");
 const mail = ["frank@gmail.com", "gb@gmail.com", "bf@gmail.com , sapienza@gmail.com"];
-let mailutente = prompt ("La tua email")
+let mailutente = prompt ("La tua email");
+
+if (mail.includes(mailutente)){
+   let element = document.createElement("div");
+   element.classList.add("sing-in");
+   console.log("bentornato");
+}
