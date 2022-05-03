d92269d7b9853d16a632440b46fcce77e312d3dd
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-03 14:04:37 +0200

    logica email inizializzata

---
+++ main.js/mail.js
@@ -0,0 +1,9 @@
+// Definisco una lista di email tramte un array
+// chiedo all'utente di identificarsi
+// costruisco un ciclo di controllo 
+//  Se l'email esiste l'utente è benvenuto 
+// Se non è registrato gli chiedo di fare in Sing Up
+ 
+
+const mail = ["frank@gmail.com", "gb@gmail.com", "bf@gmail.com , sapienza@gmail.com"];
+let mailutente = prompt ("La tua email")
